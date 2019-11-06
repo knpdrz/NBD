f393a0baacd32bcfafac405a7c6a982326d9a1b4
Monika Ruszczyk <ruszczyk.monika@gmail.com>
2019-11-06 19:27:03 +0100

    MR1: working solution

--- zapytanie_1_MR.js
+++ zapytanie_1_MR.js
@@ -1,1 +1,36 @@
-testtest
+let reduceFun = function (sex, values) {
+    result = {height: 0, weight: 0, count: 0};
+    for (var i = 0; i < values.length; i++) {
+        result.height += values[i].height;
+        result.weight += values[i].weight;
+        result.count += values[i].count;
+    }
+    return result;
+};
+
+let finalizeFun = function (sex, reducedVal) {
+    return {"avg_height": reducedVal.height / reducedVal.count, avg_weight: reducedVal.weight / reducedVal.count};
+};
+
+
+let mapFun = function () {
+    let value = {
+        height: parseFloat(this.height),
+        weight: parseFloat(this.weight),
+        count: 1
+    };
+    emit(this.sex, value);
+};
+
+db.people.mapReduce(
+    mapFun,
+    reduceFun,
+    {
+        out: "height_and_weight_sum_by_sex",
+        finalize: finalizeFun
+    }
+)
+;
+
+printjson(db.height_and_weight_sum_by_sex.find().toArray());
+
