f2d54affbf450c27b8e9b60ff1bc364c6b3891f1
Monika Ruszczyk <ruszczyk.monika@gmail.com>
2019-11-13 20:43:45 +0100

    A5: done

---
+++ zapytanie_5_A.js
@@ -0,0 +1,32 @@
+/*db.people.find().forEach(function (person) {
+    person.credit.forEach(function (credit) {
+        credit.balance = parseFloat(credit.balance);
+    });
+    db.people.save(person);
+
+});*/
+
+printjson(db.people.aggregate([
+    {
+        $match: {
+            sex: "Female",
+            nationality: "Poland"
+        }
+    },
+    {
+        $project: {
+            "credit": 1
+        }
+    },
+    {$unwind: "$credit"},
+    {
+        $group:
+            {
+                _id: "$credit.currency",
+                balance_sum: {$sum: "$credit.balance"},
+                avg_balance: {$avg: "$credit.balance"}
+
+            }
+    },
+    {$sort: {_id: 1}}
+]).toArray());
